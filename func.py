2dabd2a77c8f49415cbec43edf7b5e48972837db
Bastien Albizzati <bastien.albizzati@gmail.com>
2025-04-20 17:48:19 +0200

    1ère version finalisée du jupyter notebook

--- func.py
+++ func.py
@@ -2,64 +2,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def extract_w(u, k1, k2, romega):# renvoie l'image correspondant à la fenêtre de centre (k1,k2)
-    M,N = u.shape
-    omegax = np.array(range(k1-romega,k1+romega+1))
-    omegay = np.array(range(k2-romega,k2+romega+1))
-    omegax = omegax % M
-    omegay = omegay % M
-    uw = u[np.ix_(omegax, omegay)]
-    
-    return uw
-
-def guided_f(p,I,romega,epsilon): # IMPLEMENTATION NAIVE DU FILTRE GUIDÉ
-    M,N = p.shape
-    nomega = (1 + 2*romega)**2 # nombre de pixels dans la fenêtre
-    a = np.zeros((M,N))
-    b = np.zeros((M,N))
-    for x in range(M):
-        for y in range(N):
-            Iw = extract_w(I, x, y, romega)
-            pw = extract_w(p, x, y, romega)
-            muk = np.mean(Iw)
-            sigmak2 = np.var(Iw)
-            pbarrek = np.mean(pw)
-            a[x,y] = ((1/nomega) * np.sum(Iw*pw) - muk * pbarrek)/(sigmak2 + epsilon)
-            b[x,y] = pbarrek - a[x,y] * muk
-
-    q = np.zeros((M,N))
-    for x in range(M):
-        for y in range(N):
-            aw = extract_w(a, x, y, romega)
-            bw = extract_w(b, x, y, romega)
-            q[x,y] = (1/nomega) * np.sum(aw * p[x,y] + bw)
-    
-    return q
-
-def average_filter2(u, r):
-    """
-    Applique un filtre moyen sur une image 2D u avec une fenêtre carrée (2r+1)x(2r+1),
-    en utilisant une image intégrale pour une implémentation rapide.
-    Les bords sont symétrisés (padding miroir).
-    """
-    
-    # Padding symétrique (miroir)
-    padded = np.pad(u, ((r+1, r), (r+1, r)), mode='symmetric')
-    
-    # Calcul de l'image intégrale (somme cumulée 2D)
-    integral = np.cumsum(np.cumsum(padded, axis=0), axis=1)
-    
-    # Zone centrale : du coin supérieur gauche au coin inférieur droit
-    A = integral[2*r+1:,2*r+1:]
-    B = integral[:-2*r-1,2*r+1:]
-    C = integral[2*r+1:,:-2*r-1]
-    D = integral[:-2*r-1,:-2*r-1]
-    
-    total = A - B - C + D
-    out = total / ((2*r + 1) ** 2)
-    
-    return out
-
 def guided_f_fast2(p,I,romega,epsilon): # IMPLEMENTATION EFFICACE DU FILTRE GUIDÉ (MATRICE INTÉGRALE)
     M,N = p.shape
     a = np.zeros((M,N))
@@ -182,6 +124,85 @@ def compare_parameters_fg_zoom(p1,ranger,rangeeps,xmin,xmax,ymin,ymax):
 
     plt.tight_layout()
     plt.show()
+
+
+#-----------------------------------------------------------
+# AUTRES FONCTIONS FINALEMENT NON UTILISÉES DANS LA VERSION FINALE DU JUPYTER NOTEBOOK
+#-----------------------------------------------------------
+
+def average_filter(u,r):
+    # uniform filter with a square (2*r+1)x(2*r+1) window 
+    # u is a 2d image
+    # r is the radius for the filter
+   
+    (nrow, ncol) = u.shape
+    big_uint = np.zeros((nrow+2*r+1,ncol+2*r+1))
+    big_uint[r+1:nrow+r+1,r+1:ncol+r+1] = u
+    big_uint = np.cumsum(np.cumsum(big_uint,0),1)  # integral image
+        
+    out = big_uint[2*r+1:nrow+2*r+1,2*r+1:ncol+2*r+1] + big_uint[0:nrow,0:ncol] - big_uint[0:nrow,2*r+1:ncol+2*r+1] - big_uint[2*r+1:nrow+2*r+1,0:ncol]
+    out = out/(2*r+1)**2
+    
+    return out
+
+def extract_w(u, k1, k2, romega):# renvoie l'image correspondant à la fenêtre de centre (k1,k2)
+    M,N = u.shape
+    omegax = np.array(range(k1-romega,k1+romega+1))
+    omegay = np.array(range(k2-romega,k2+romega+1))
+    omegax = omegax % M
+    omegay = omegay % M
+    uw = u[np.ix_(omegax, omegay)]
+    
+    return uw
+
+def guided_f(p,I,romega,epsilon): # IMPLEMENTATION NAIVE DU FILTRE GUIDÉ
+    M,N = p.shape
+    nomega = (1 + 2*romega)**2 # nombre de pixels dans la fenêtre
+    a = np.zeros((M,N))
+    b = np.zeros((M,N))
+    for x in range(M):
+        for y in range(N):
+            Iw = extract_w(I, x, y, romega)
+            pw = extract_w(p, x, y, romega)
+            muk = np.mean(Iw)
+            sigmak2 = np.var(Iw)
+            pbarrek = np.mean(pw)
+            a[x,y] = ((1/nomega) * np.sum(Iw*pw) - muk * pbarrek)/(sigmak2 + epsilon)
+            b[x,y] = pbarrek - a[x,y] * muk
+
+    q = np.zeros((M,N))
+    for x in range(M):
+        for y in range(N):
+            aw = extract_w(a, x, y, romega)
+            bw = extract_w(b, x, y, romega)
+            q[x,y] = (1/nomega) * np.sum(aw * p[x,y] + bw)
+    
+    return q
+
+def average_filter2(u, r):
+    """
+    Applique un filtre moyen sur une image 2D u avec une fenêtre carrée (2r+1)x(2r+1),
+    en utilisant une image intégrale pour une implémentation rapide.
+    Les bords sont symétrisés (padding miroir).
+    """
+    
+    # Padding symétrique (miroir)
+    padded = np.pad(u, ((r+1, r), (r+1, r)), mode='symmetric')
+    
+    # Calcul de l'image intégrale (somme cumulée 2D)
+    integral = np.cumsum(np.cumsum(padded, axis=0), axis=1)
+    
+    # Zone centrale : du coin supérieur gauche au coin inférieur droit
+    A = integral[2*r+1:,2*r+1:]
+    B = integral[:-2*r-1,2*r+1:]
+    C = integral[2*r+1:,:-2*r-1]
+    D = integral[:-2*r-1,:-2*r-1]
+    
+    total = A - B - C + D
+    out = total / ((2*r + 1) ** 2)
+    
+    return out
+
 
 
 
